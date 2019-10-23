ee74cc204de221cebccf873c806e24baa3ecada3
Mary M. Zysk <doumkatek42@gmail.com>
2019-10-22 23:31:46 -0400

    Homework

---
+++ Session2/session2_homework/S2_Homework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace S2_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SayMyName.NameSayer());
+            Console.Write("Press enter to close");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Session2/session2_homework/S2_Homework/SayMyName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2_Homework
+{
+    class SayMyName
+    {
+
+        public static string nameToSay { get; set; } = "Mary";
+
+        public static string NameSayer()
+        {
+            return "Hello " + nameToSay;
+        }
+    }
+}
