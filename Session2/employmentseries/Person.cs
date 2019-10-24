456063ef14cda5a7feb62e9a9c662dca343f0671
TzvetomirTodorov <tzvtdr@gmail.com>
2019-10-24 01:51:14 -0400

    Week two homework completion.

---
+++ Session2/employmentseries/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace employmentseries
+{
+    public class Person
+    {
+        public void PrintMyName(string name)
+        {
+            Console.WriteLine($"Hello World! My name is {name}!");
+        }
+    }
+}
--- Session2/employmentseries/Program.cs
+++ Session2/employmentseries/Program.cs
@@ -6,7 +6,8 @@ namespace employmentseries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person myPerson = new Person();
+            myPerson.PrintMyName("Tzvetomir");
         }
     }
 }
